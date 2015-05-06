e8bb9f31be24c1e3d1415c931cbe9c94c9e1c653
Alpha Chau <chaua0927@gmail.com>
2015-05-06 23:16:44 +0200

    Testing Round 4

--- app/src/main/java/hu/ait/android/cevicheteam/ceviche/MainActivity.java
+++ app/src/main/java/hu/ait/android/cevicheteam/ceviche/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("DEBUG", "Frogs rogs...");
+        Log.d("DEBUG", "Frogs rogs cogs...");
     }
 
     @Override
