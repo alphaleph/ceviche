b471e8a9c9d3181d9fe1eac615ea3c6bc2aacb44
umanduj2 <uriel.mandujano14@gmail.com>
2015-05-12 15:22:24 +0200

    Basic Swipe Functionality Implemented

--- app/src/main/java/hu/ait/android/cevicheteam/ceviche/SwipeActivity.java
+++ app/src/main/java/hu/ait/android/cevicheteam/ceviche/SwipeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 
 public class SwipeActivity extends AppCompatActivity {
